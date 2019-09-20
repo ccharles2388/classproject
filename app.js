3db0652b9575c285bd75ee085338043dabc6fce7
Curtis Charles <ccharles2388@gmail.com>
2019-09-19 21:07:38 -0400

    javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBC9EdzFuCmH14viAgGa7joQBxcr2H_a1c",
+    authDomain: "the-wild-music-experience.firebaseapp.com",
+    databaseURL: "https://the-wild-music-experience.firebaseio.com",
+    projectId: "the-wild-music-experience",
+    storageBucket: "",
+    messagingSenderId: "885036588462",
+    appId: "1:885036588462:web:b52fe4c987259292e67c3f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
